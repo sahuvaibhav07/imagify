32fca7e263639442d3e918757d5f9b0bae39f288
sahuvaibhav07 <sahuvaibhav06@gmail.com>
2026-03-11 14:56:38 +0530

    upload imagify project

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,36 +1,22 @@
-<<<<<<< HEAD
-import React from "react"
-import { Routes, Route } from "react-router-dom"
-import Home from "./pages/Home"
-import Result from "./pages/Result"
-import BuyCredit from "./pages/BuyCredit"
-import Navbar from "./components/Navbar"
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Result from "./pages/Result";
+import BuyCredit from "./pages/BuyCredit";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
-    <div className='px-4 sm:px-10 md:px-14 lg:px-28 min-h-screen bg-gradint-to-b from-teal-50
-    to-orange-50' >
+    <div className="px-4 sm:px-10 md:px-14 lg:px-28 min-h-screen bg-gradient-to-b from-teal-50 to-orange-50">
+      <Navbar />
 
-      <Navbar/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/result" element={<Result />} />
         <Route path="/buy" element={<BuyCredit />} />
       </Routes>
-=======
-import React from 'react'
-
-const App = () => {
-  return (
-    <div>
-      
->>>>>>> 60e22ffd06d408173c82f76ae1c8ac6b9c5c7df1
     </div>
-  )
-}
+  );
+};
 
-<<<<<<< HEAD
-export default App
-=======
-export default App
->>>>>>> 60e22ffd06d408173c82f76ae1c8ac6b9c5c7df1
+export default App;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,8 @@
-<<<<<<< HEAD
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import AppContextProvider from "./context/AppContext.jsx";
-import "./index.css";
 import App from "./App.jsx";
+import "./index.css";
+import AppContextProvider from "./context/AppContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
@@ -12,15 +11,3 @@ createRoot(document.getElementById("root")).render(
     </AppContextProvider>
   </BrowserRouter>
 );
-=======
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
->>>>>>> 60e22ffd06d408173c82f76ae1c8ac6b9c5c7df1
